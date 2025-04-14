f3b30bb5aad0ac253edcc00f490ebe5a9f0ce0de
Shloksp1422 <sjs220010@utdallas.edu>
2025-04-13 19:22:49 -0500

    Update config.js 1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_fyfXl7pQ0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7t53gcivuu7o3l8k9vt62ruje5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3o0bf9rc54u25b85617b4qi31s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
